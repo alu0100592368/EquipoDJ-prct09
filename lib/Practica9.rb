f3046542a86600bf25b0531f927ebfce7b8438b6
Javier Mena <alu0100454741@ull.edu.es>
2013-11-15 12:45:05 +0000

    modificaciones

--- lib/Practica9.rb
+++ lib/Practica9.rb
@@ -24,7 +24,6 @@ class Matriz
       		print "#{matriz[i][j]}  "
        	end
       end
-       
    end
 
    def [](i,j)
@@ -33,49 +32,48 @@ class Matriz
 
 end
 
-
-
 #Clase de la matriz Dispersa
 class MatrizDispersa < Matriz
-  	
-  	attr_reader :hash_no_ceros
+  
+  attr_reader :hash_no_ceros
 
-  	def initialize(matriz)
-  		#comprobamos que la matriz es dispersa o no
-  		n_elementos= (matriz.size * matriz[0].size)*0.6 
-  		@filas = matriz.size
-      	@columnas = matriz[0].size
-      	n_ceros=0
-      	@hash_no_ceros={}
-      	filas.times do |i|
-      		columnas.times do |j|
-      			if (matriz[i][j]==0)  
-      				n_ceros=n_ceros+1
-              	else
-                 	#hash
-                 	pos_no_cero="#{i}#{j}"
-                 	@hash_no_ceros[pos_no_cero]=matriz[i][j] 
-              	end
-        	end
-  		end
-  	end
+  def initialize(matriz)
+    #comprobamos que la matriz es dispersa o no
+    n_elementos= (matriz.size * matriz[0].size)*0.6 
+    @filas = matriz.size
+    @columnas = matriz[0].size
+    n_ceros=0
+    @hash_no_ceros={}
+    filas.times do |i|
+      columnas.times do |j|
+        if (matriz[i][j]==0)  
+          n_ceros=n_ceros+1
+        else
+          #hash
+          pos_no_cero="#{i}#{j}"
+          @hash_no_ceros[pos_no_cero]=matriz[i][j] 
+        end
+      end
+    end
+  end
   	
-  	def to_s
-  		filas.times do |i|
-         columnas.times do |j|
-            if (hash_no_ceros.key?("#{i}#{j}"))
-               print hash_no_ceros["#{i}#{j}"]
-               print "  "
-            else
-               print "0  "
-            end
-         end
-         puts
-       end  	
-  	end
+  def to_s
+    filas.times do |i|def +(other)
+    
+  end
+      columnas.times do |j|
+        if (hash_no_ceros.key?("#{i}#{j}"))
+          print hash_no_ceros["#{i}#{j}"]
+          print "  "
+        else
+          print "0  "
+        end
+      end
+    end  	
+  end
 
-  	def +(other)
+  def +(other)
   	
-  	end
+  end
   	
 end
